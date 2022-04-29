b5925fb7e57f113009e240fce4adab0c0753dd4a
JJKid <jj.arroyo@ciencias.unam.mx>
2022-04-29 02:09:10 -0500

    Added documentation and improved GUI content

--- README.txt
+++ README.txt
@@ -3,10 +3,18 @@ Arroyo Rivera Juan José 416053223
 
 Ejecución del programa:
 
-Instalar python3
-En la carpeta /src ejecutar: python .\steganography.py
-Seguir las instrucciones en pantalla. Teniendo cuidado con poner la extensión de los
+1. Instalar python3
+2. Instalar biblioteca Pillow (mas referencia https://pillow.readthedocs.io/en/stable/installation.html)
+
+	python3 -m pip install --upgrade pip
+	python3 -m pip install --upgrade Pillow
+
+3. Para llevar a cabo el procedimiento de ocultar o develar:
+Moverse a la carpeta /src y ejecutar: python .\steganography.py 
+
+4. Seguir las instrucciones en pantalla. Teniendo cuidado con poner la extensión de los
 nombres de archivos indicada en cada paso
+
 Los archivos de entrada (imagen original y texto a ocultar) estan en la carpeta /data
 y los archivos de salida se guardan en la carpeta /out
 
--- src/steganography.py
+++ src/steganography.py
@@ -1,54 +1,76 @@
 import os
 from PIL import Image
-class Steganography:
-    def __init__(self, image):
-        self.image = image
 
 def convertDecimalToBinary(num):
+    """ 
+    Converts a decimal number to a string with its binary representation of 8 bits
+
+    """
     return '{0:08b}'.format(num)
 
 def convertBinaryToChar(binaryNum):
+    """ 
+    Converts a string with an 8-bit binary number 
+    to a decimal number and then to the char asociated to this decimal number in ASCII
+
+    """
     return chr(int(binaryNum,2))
 
-def convertBinaryToDecimal(num): 
+def convertBinaryToDecimal(num):
+    """ 
+    Converts a string with an 8-bit binary number to a decimal number
+    
+    """ 
     return int(num, 2) 
 
 def convertTextToBinary(text):
-    # Convert each character in the string of text
-    # to a binary string
-    # Return all binary strings joined into one string
+    """ 
+    Converts each character in a string of text to a binary string
+    and returns a string with all binary strings joined into one string
+
+    """
+    # 
     if type(text) is str:
         return ''.join([convertDecimalToBinary(ord(c)) for c in text])
 
 def hideText():
+    """ 
+    Hide text from a text file into an PNG image 
+    with LSB steganography technique
+
+    Overwrites the last bit of the binary representation of 
+    the number asocciated with a color (R,B,G or A) on each pixel
+    until the all input text converted to binary (with a delimiter at the end)
+    gets saved
+
+    Creates a PNG file with the text hidden, saves it on /out 
+
+    """
     scriptDir = os.path.dirname(__file__)
-    imageFileName = input("Enter original image file name (with .png extension) \n")
+    defaultImageFileName = "original.png"  
+    defaultTextFileName = "input.txt"
+    defaultOutputImageFileName = "modified.png"
+    imageFileName = input(f"Enter original image file name (with .png extension). Press enter to use default image file: {defaultImageFileName} \n")
+    if not imageFileName:
+        imageFileName = defaultImageFileName
     imagePath = os.path.join(scriptDir, '../data/', imageFileName)
     image = Image.open(imagePath) 
     print("Image details: ", image.format, image.size, image.mode)
 
-    textFileName = input("Enter text file name with text to hide (with .txt extension) \n")
+    textFileName = input(f"Enter text file name with text to hide (with .txt extension). Press enter to use default text file: {defaultTextFileName} \n")
+    if not textFileName:
+        textFileName = defaultTextFileName
     textFilePath = os.path.join(scriptDir, '../data/', textFileName)
     with open(textFilePath, 'r') as textFile:
         text = textFile.read().replace('\n', ' ')
 
-    outputImageFileName = input("Enter output image file name (with .png extension) \n")
+    outputImageFileName = input(f"Enter output image file name (with .png extension). Press enter to use default output text file name: {defaultOutputImageFileName} \n")
+    if not outputImageFileName:
+        outputImageFileName = defaultOutputImageFileName
     
     textInBinary = convertTextToBinary(text) + '1111111111111110'
     currDigitIndex = 0
-    # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
-    pixelMap = image.load()
-    testPixel = pixelMap[50,50]
-    print("testPixel", testPixel)
-    rInBinary = convertDecimalToBinary(testPixel[0])
-    rInbinaryModified = rInBinary[:-1] + '1'
-    # Test if last character is different before doing an overwrite on LSB
-    print("r in binary" , rInBinary)
-    print("r modified in binary" , rInbinaryModified)
-    print("r back to decimal", convertBinaryToDecimal(rInBinary))
-    print("r modified back to decimal", convertBinaryToDecimal(rInbinaryModified))
-
-    listofbinaries = []
+    pixelsGrid = image.load()    
     for i in range(image.size[0]):
         for j in range(image.size[1]):  
             r, g, b, a = image.getpixel((i, j))
@@ -68,41 +90,50 @@ def hideText():
                 aInbinaryModified = convertDecimalToBinary(a)[:-1] + textInBinary[currDigitIndex]
                 a = convertBinaryToDecimal(aInbinaryModified)
                 currDigitIndex+=1
-            listofbinaries.append((rInbinaryModified, bInbinaryModified, gInbinaryModified, aInbinaryModified))
-            pixelMap[i,j] = (r,g,b,a)
+            pixelsGrid[i,j] = (r,g,b,a)
+
             if currDigitIndex == len(textInBinary):
-                print(" Break Ultimo indice", currDigitIndex, i,j)
                 break  
         else:
-            # Continue to next i iteration if inner for loop finish 
             continue  
         break    
     
     outputImagePath = os.path.join(scriptDir, '../out/', outputImageFileName)
-    image.save(outputImagePath)
-    image.show()
-    
-    
+    image.save(outputImagePath)    
+    print("Modified image saved on " + outputImagePath)
     
 def unveilText():
-    # Iterate over each pixel 
-    # Extract up to 4 LSB and save it into a currExtractingBinaryString
-    # Once we found that the last 16 binary digits are equal to the 
-    # delimiter we stop reading more pixels
-    # For each string of binary chars of length == 8 in currExtractingBinaryString,
-    # we convert it into its char representation and append it to unveiledText string
+    """ 
+    Unveils hidden text inside a image file by LSB steganography technique
     
-    # We save the unveiledText string to an output text file 
-    scriptDir = os.path.dirname(__file__)
 
-    imageFileName = input("Enter image file name with hidden text (with .png extension) \n")
+    Iterate over each pixel and save the last bit of the binary representation
+    of each color and save it into a string which contains all this binary numbers extracted
+
+    Once we found that the last 16 binary digits are equal to the 
+    delimiter we stop reading more pixels
+
+    Finally we convert each binary number of length 8 into its char representation 
+    and append it to unveiledText string , which is written to output text file
+
+    Creates a text file which contains the unveiled text from image, saves it on /out
+
+    """    
+    
+    scriptDir = os.path.dirname(__file__)
+    defaultOutputImageFileName = "modified.png"
+    defaultOutputTextFileName = "output.txt"
+    imageFileName = input(f"Enter image file name with hidden text (with .png extension). Press enter to use previous default output image file: {defaultOutputImageFileName} \n")
+    if not imageFileName:
+        imageFileName = defaultOutputImageFileName
     imagePath = os.path.join(scriptDir, '../out/', imageFileName)
     image = Image.open(imagePath) 
     print("Image details: ", image.format, image.size, image.mode)
 
-    outputTextFileName = input("Enter output text file name (with .txt extension) \n")
+    outputTextFileName = input(f"Enter output text file name (with .txt extension). Press enter to use default output text file name: {defaultOutputTextFileName} \n")
+    if not outputTextFileName:
+        outputTextFileName = defaultOutputTextFileName
 
-    listofbinaries = []
     currExtractingBinaryString = ''
     delimiter = '1111111111111110'
     unveiledText = ''
@@ -115,12 +146,11 @@ def unveilText():
             currExtractingBinaryString+= convertDecimalToBinary(a)[-1]
             if len(currExtractingBinaryString) >= len(delimiter):
                 if currExtractingBinaryString[-16:] == delimiter:
-                    print("Delimiter found", len(currExtractingBinaryString) , currExtractingBinaryString)
                     break
         else:
-            # Continue to next i iteration if inner for loop finish 
             continue  
         break  
+
     if len(currExtractingBinaryString) % 8 == 0:
         for i in range(0,len(currExtractingBinaryString),8):
             unveiledText+= convertBinaryToChar(currExtractingBinaryString[i: i+8])
@@ -129,14 +159,13 @@ def unveilText():
     
     print("Unveiled text: ", unveiledText[:-2])
     outputTextFilePath = os.path.join(scriptDir, '../out/', outputTextFileName)
-    print("Saved on file " + outputTextFileName)
-
+    print("Unveiled text saved on " + outputTextFilePath)
 
     with open(outputTextFilePath, "w") as textFile:
         textFile.write(unveiledText[:-2])
 
 def main():
-    optionSelected = input("Enter \n h to hide \n u to unveil \n")
+    optionSelected = input("Type \n 1. h to hide \n 2. u to unveil \n Then press enter \n")
     if optionSelected == "h":
         hideText()
     if optionSelected == "u":
